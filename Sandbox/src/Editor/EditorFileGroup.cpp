67f195704ac4f38e794c55d3d656262e4b0e0cb8
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2021-06-23 12:44:12 +0800

    reset the file path

--- Sandbox/src/Editor/EditorFileGroup.cpp
+++ Sandbox/src/Editor/EditorFileGroup.cpp
@@ -18,7 +18,7 @@ Technology is prohibited.
 
 #include <windows.h>
 
-std::string FileGroup::s_rootPath = "C:/Users/User/Desktop/New folder (3)/";
+std::string FileGroup::s_rootPath = "./";
 std::string FileGroup::s_CurrentPath = s_rootPath;
 std::string FileGroup::s_hoveredPath = s_rootPath;
 std::string FileGroup::s_selectedpath;//path with the itemname
