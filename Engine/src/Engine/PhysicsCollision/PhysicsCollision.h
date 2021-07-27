1eba267432227e2a92b25549417b7daae3560f7e
Spironan <c_tecklee@hotmail.com>
2021-07-27 22:06:03 +0800

    Added Comments to Physics Files. Ready for PR

--- Engine/src/Engine/PhysicsCollision/PhysicsCollision.h
+++ Engine/src/Engine/PhysicsCollision/PhysicsCollision.h
@@ -1,3 +1,16 @@
+/************************************************************************************//*!
+\file          PhysicsCollision.h
+\project       <PROJECT_NAME>
+\author        Chua Teck Lee, c.tecklee, 390008420
+\par           email: c.tecklee\@digipen.edu
+\date          July 27, 2021
+\brief         Relevant files Required for Physics and Collision Implemention
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
 #pragma once
 
 
--- Engine/src/Engine/PhysicsCollision/PhysicsSystem.h
+++ Engine/src/Engine/PhysicsCollision/PhysicsSystem.h
@@ -1,3 +1,18 @@
+/************************************************************************************//*!
+\file          PhysicsSystem.h
+\project       <PROJECT_NAME>
+\author        Chua Teck Lee, c.tecklee, 390008420
+\par           email: c.tecklee\@digipen.edu
+\date          July 27, 2021
+\brief         Describes a Physics System that applies dynamics to all physics objects
+               performs collision detection between physics objects and 
+               resolve Physics based Collision resolution.
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
 #pragma once
 
 #include "Engine/ECS/System.h"
@@ -9,11 +24,24 @@ namespace engine
     class PhysicsSystem : public System
     {
     public:
+        /*********************************************************************************//*!
+        \brief    Default constructor
+         
+        \param    ECS_Manager
+                  The Manager that controls of the systems
+        *//**********************************************************************************/
         PhysicsSystem(ECS_Manager& ECS_Manager) 
             : System { ECS_Manager } 
             , Gravity { 0.f, -9.81f }
         {};
 
+
+        /*********************************************************************************//*!
+        \brief    Updates the Physics System everyframe
+         
+        \param    deltaTime
+                    Amount of time passed since the previous frame.
+        *//**********************************************************************************/
         void Update(Timestep deltaTime);
         
         glm::vec2 Gravity;
