d2f9557a6ad545b44be6fec5b5692dc770240ff1
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2021-07-28 02:37:52 +0800

    Can ctrl+z and ctrl+v in inspector

--- Sandbox/src/Editor/ActionStack/InspectorActionBehaviour.h
+++ Sandbox/src/Editor/ActionStack/InspectorActionBehaviour.h
@@ -1,17 +1,38 @@
 #pragma once
 #include "ActionBehaviour.h"
 #include "Engine/ECS/ECS_Utility.h"
+#include "Engine/Transform/Transform3D.h"
 
 #include <rttr/property.h>
 #include <rttr/variant.h>
+template <typename Component>
 class InspectorActionBehaviour : public ActionBehaviour
 {
 public:
 	InspectorActionBehaviour(const std::string& str,engine::Entity ett,const rttr::property& p, rttr::variant& _undo,rttr::variant& _redo) 
-		:ActionBehaviour{str}, item(ett), prop(p), undoData(_undo),redoData(_redo) {}
-	virtual ~InspectorActionBehaviour()override;
-	void undo()override;
-	void redo()override;
+		:ActionBehaviour{str}, item(ett), prop(p), undoData(_undo),redoData(_redo) 
+	{
+
+	}
+/*********************************************************************************//*!
+\brief    
+	function for cleaning the data assigned
+	some data structures might be too complex
+	that just doing a delete on a void* will not work
+*//**********************************************************************************/
+	virtual ~InspectorActionBehaviour()override
+	{
+			undoData.clear();
+			redoData.clear();
+	}
+	void undo()override
+	{
+		prop.set_value(static_cast<engine::GameObject>(item).GetComponent<Component>(), undoData);
+	}
+	void redo()override
+	{
+		prop.set_value(static_cast<engine::GameObject>(item).GetComponent<Component>(), redoData);
+	}
 
 private:
 	engine::Entity item;
@@ -19,4 +40,5 @@ private:
 	rttr::variant undoData;
 	rttr::variant redoData;
 };
+
 
