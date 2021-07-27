b347157baf00351cb3c87abd4d7cc0b1f038dbba
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2021-07-28 01:58:00 +0800

    inspector view can show all the transform components

--- Sandbox/src/Editor/InspectorView.cpp
+++ Sandbox/src/Editor/InspectorView.cpp
@@ -5,24 +5,16 @@
 #include "ActionStack/InspectorActionBehaviour.h"
 
 //engine code
+#include "glm/gtc/type_ptr.hpp"
 #include "Engine/Transform/Transform3D.h"
 #include "Engine/ECS/GameObject.h"
-
+#include "Engine/ECS/WorldManager.h"
 //libs
 #include <imgui.h>
 #include <imgui_internal.h>
 #include <rttr/type>
 #include <string>
-enum : int
-{
-	enum_INT,
-	enum_FLOAT,
-	enum_STRING,
-	enum_BOOL,
-	type_ENTITY,
-	type_VEC3,
-	type_MAT4,
-};
+
 
 InspectorView::InspectorView()
 {
@@ -45,8 +37,11 @@ void InspectorView::Show()
 		}
 		else
 		{
-			ImGui::Text("Name :  %s", static_cast<engine::GameObject>(ObjectGroup::s_FocusedObject).Name);
-			//ReadComponents(ObjectGroup::s_FocusedObject->get_type());
+			auto& go = static_cast<engine::GameObject>(ObjectGroup::s_FocusedObject);
+			
+			ImGui::Text("Name :  %s", go.Name.c_str());
+			
+			ReadComponents(go.GetComponent<engine::Transform3D>());//hard coded to read only 1 component
 			ImGui::EndChild();
 		}
 	}
@@ -71,68 +66,4 @@ void InspectorView::Show()
 	ImGui::End();
 }
 
-void InspectorView::ReadComponents(const rttr::type& _type)
-{
-	std::vector<rttr::property> types = _type.get_properties();
 
-	ImGui::BeginChild(_type.get_name().c_str(), { 0,types.size() * 30.0f }, true);
-	rttr::variant current_value;
-
-	for (const rttr::property& element : types)
-	{
-		current_value.clear();
-		const rttr::type::type_id id = element.get_type().get_id();
-		if (id == m_tracked_ids[enum_INT])
-		{
-			int value = element.get_value(ObjectGroup::s_FocusedObject).get_value<int>();
-			current_value = value;
-			if (ImGui::DragInt(element.get_name().c_str(), &value))
-			{
-				element.set_value(ObjectGroup::s_FocusedObject, value);
-
-			}
-		}
-		else if (id == m_tracked_ids[enum_FLOAT])
-		{
-			float value = element.get_value(ObjectGroup::s_FocusedObject).get_value<float>();
-			current_value = value;
-			if (ImGui::DragFloat(element.get_name().c_str(), &value))
-			{
-				element.set_value(ObjectGroup::s_FocusedObject, value);
-			}
-		}
-		else if (id == m_tracked_ids[enum_STRING])
-		{
-			static char buf[100];
-			std::string value = element.get_value(ObjectGroup::s_FocusedObject).get_value<std::string>();
-			current_value = value;
-			strcpy(buf, value.data());
-			if (ImGui::InputText(element.get_name().c_str(),buf,100,ImGuiInputTextFlags_EnterReturnsTrue| ImGuiInputTextFlags_NoUndoRedo))
-			{
-				value = buf;
-				element.set_value(ObjectGroup::s_FocusedObject, value);
-				current_value = value;
-			}
-		}
-		//undo and redo instructions
-		{
-			static rttr::variant undo;
-			static rttr::variant redo;
-			if (ImGui::IsItemClicked(ImGuiMouseButton_Left))
-			{
-				undo.clear();
-				undo = current_value;
-			}
-			if(ImGui::IsItemDeactivatedAfterEdit())
-			{
-				//redo stack
-				redo.clear();
-				redo = current_value;
-				std::string temp = "Change value of element: " + element.get_name() + " of " + static_cast<engine::GameObject>(ObjectGroup::s_FocusedObject).Name;
-				ActionStack::AllocateInBuffer(new InspectorActionBehaviour{temp, ObjectGroup::s_FocusedObject, element, undo, redo });
-			}
-		}
-		
-	}
-	ImGui::EndChild();
-}
