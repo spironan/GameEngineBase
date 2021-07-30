8526b3198c5982e7e34ea84ae5986c7530e6f7b8
Spironan <c_tecklee@hotmail.com>
2021-07-31 00:32:26 +0800

    Physics Test Layer Update

--- Sandbox/src/TestLayers/PhysicsTestLayer.h
+++ Sandbox/src/TestLayers/PhysicsTestLayer.h
@@ -13,58 +13,88 @@ Technology is prohibited.
 *//*************************************************************************************/
 #pragma once
 
-#include "Engine.h"
-#include "UtilityLayers/SceneBaseLayer.h"
+#include <Engine.h>
 
-class PhysicsTestLayer final : public SceneBaseLayer
+class PhysicsTestLayer : public engine::Layer
 {
 private:
-    engine::GameObject m_cam;
+    engine::World& m_world;
+    engine::OrthographicCamera cam{ -1, 1, -1, 1 };
     int width{}, height{};
+    engine::GameObject m_root, m_second;
 
     oom::vec2 upperbounds, lowerbounds;
 public:
 
     PhysicsTestLayer()
-        : SceneBaseLayer{ "PhysicsTestLayer" }
-        , upperbounds{ 200,  200 }
-        , lowerbounds{ -200, -200 }
-    {
-    }
-
-    void Init() final override
+        : Layer{ "PhysicsTestLayer" }
+        , m_world{ engine::WorldManager::CreateWorld() }
+        , upperbounds { 200,  200 }
+        , lowerbounds {-200, -200 }
     {
         engine::Window& x = engine::Application::Get().GetWindow();
         width = x.GetSize().first;
         height = x.GetSize().second;
+        cam.SetProjection(-width / 2.0f, width / 2.0f, -height / 2.0f, height / 2.0f);
 
-        m_cam = CreateGameObject();
-        auto& cam = m_cam.AddComponent<engine::SceneCamera>();
-            
-        auto& rs = m_scene.GetWorld().RegisterSystem<engine::Renderer2DSystem>(cam);
-        auto& ps = m_scene.GetWorld().RegisterSystem<engine::PhysicsSystem>();
+        auto& ts = m_world.RegisterSystem<engine::TransformSystem>();
+        auto& rs = m_world.RegisterSystem<engine::Renderer2DSystem>(cam);
+        auto& ps = m_world.RegisterSystem<engine::PhysicsSystem>();
 
-        engine::Texture tex = engine::TextureLoader::LoadFromFilePath("../Engine/assets/images/ogre.png");
-        engine::TextureDatabase::AddTexture("ogre", tex);
+        {
+            m_root.Transform.Scale() = { 50.f, 50.f, 1.0f };
+            m_root.AddComponent<engine::Sprite2D>();
+            m_root.AddComponent<engine::BoxCollider2D>();
+            auto& pc = m_root.AddComponent<engine::Rigidbody2D>();
+            pc.SetMass(1.f);
+            pc.GravityScale = 0.0f;
+        }
+        
+        {
+            m_second.Transform.Position() = { 100.f, 0.f, 0.f };
+            m_second.Transform.Scale() = { 50.f, 50.f, 1.0f };
+            m_second.AddComponent<engine::Sprite2D>();
+            m_second.AddComponent<engine::BoxCollider2D>();
+            auto& pc = m_second.AddComponent<engine::Rigidbody2D>();
+            pc.SetMass(1.f);
+            pc.GravityScale = 0.0f;
+        }
 
-        auto& go = CreateGameObject();
-        go.Transform().Scale() = { 50.f, 50.f, 1.0f };
-        auto& rb = go.AddComponent<engine::RigidBody>();
-        rb.SetMass(100.f);
-        auto& rootSpr = go.AddComponent<engine::Sprite2D>();
-        rootSpr.SetTexture(tex);
     }
+
 
     virtual void OnUpdate(engine::Timestep dt) override
     {
-        engine::SceneManager::SetActiveScene(m_scene.GetID());
-        m_scene.GetWorld().GetSystem<engine::TransformSystem>()->Update();
-        m_scene.GetWorld().GetSystem<engine::PhysicsSystem>()->Update(dt);
-        
-        // transform objects
-        auto view = m_scene.GetWorld().GetComponentView<engine::Transform3D>();
-        for (auto& [transform] : view)
+        m_world.GetSystem<engine::TransformSystem>()->Update();
+        m_world.GetSystem<engine::PhysicsSystem>()->Update(dt);
+
+        if (engine::Input::IsKeyPressed(ENGINE_KEY_UP))
         {
+            m_root.GetComponent<engine::Rigidbody2D>().AddForce(glm::vec2{ 0, 50 });
+        }
+        
+        if (engine::Input::IsKeyPressed(ENGINE_KEY_DOWN))
+        {
+            m_root.GetComponent<engine::Rigidbody2D>().AddForce(glm::vec2{ 0,-50 });
+        }
+        
+        if (engine::Input::IsKeyPressed(ENGINE_KEY_RIGHT))
+        {
+            m_root.GetComponent<engine::Rigidbody2D>().AddForce(glm::vec2{ 50,0 });
+        }
+        
+        if (engine::Input::IsKeyPressed(ENGINE_KEY_LEFT))
+        {
+            m_root.GetComponent<engine::Rigidbody2D>().AddForce(glm::vec2{ -50,0 });
+        }
+        auto force = m_root.GetComponent<engine::Rigidbody2D>().GetForce();
+        //LOG_TRACE("{0}{1}", force.x, force.y );
+
+        // transform objects
+        auto view = m_world.GetComponentView<engine::Transform3D>();
+        for (auto& ent : view)
+        {
+            auto& transform = m_world.GetComponent<engine::Transform3D>(ent);
             if (transform.Position().y < lowerbounds.y)
             {
                 transform.Position().y = upperbounds.y;
@@ -87,7 +117,7 @@ public:
 
     virtual void OnImGuiRender() override
     {
-        m_scene.GetWorld().GetSystem<engine::Renderer2DSystem>()->Update();
+        m_world.GetSystem<engine::Renderer2DSystem>()->Update();
     }
 
 };
