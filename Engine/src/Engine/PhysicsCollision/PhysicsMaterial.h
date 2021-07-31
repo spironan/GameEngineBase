088fc01e061e6b571bf310eece39151166219a5f
Spironan <c_tecklee@hotmail.com>
2021-07-31 23:07:15 +0800

    Physics Material

---
+++ Engine/src/Engine/PhysicsCollision/PhysicsMaterial.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace engine
+{
+    struct PhysicsMaterial
+    {
+        float Density = 1.0f;
+        float Restitution = 0.f;
+        float DynamicFriction = 0.6f;
+        float StaticFriction = 0.6f;
+    };
+}
