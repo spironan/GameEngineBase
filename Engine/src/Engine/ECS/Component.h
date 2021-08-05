6f9980524c948ea81166b3b5fa9f6406615ad901
Spironan <c_tecklee@hotmail.com>
2021-08-05 22:33:27 +0800

    Updates to GameObject and added Helper functions to Components

--- Engine/src/Engine/ECS/Component.h
+++ Engine/src/Engine/ECS/Component.h
@@ -12,19 +12,32 @@ namespace engine
         Component& operator=(const Component&) = default;
         virtual ~Component() = default;
 
-        Component(Entity entity, bool active = true) : m_entity(entity), m_active(active) {}
+        Component(Entity entity, bool active) : m_entity(entity), m_active(active) {}
 
-        Entity GetEntity()       const { return m_entity; }
-        void SetEntity(Entity entity)  { m_entity = entity; }
-        constexpr bool IsActive()    const { return m_active; }
+        Entity GetEntity() const            { return m_entity; }
+        void SetEntity(Entity entity)       { m_entity = entity; }
+        constexpr bool IsActive() const     { return m_active; }
 
         void SetActive(bool _active) { m_active = _active; }
 
         template<typename T, typename T2 = engine::WorldManager>
-        T& GetComponent()
+        T& GetComponent() const
         {
             return T2::GetActiveWorld().GetComponent<T>(m_entity);
         }
+        
+        template<typename T, typename T2 = engine::WorldManager>
+        T* TryGetComponent() const
+        {
+            return T2::GetActiveWorld().TryGetComponent<T>(m_entity);
+        }
+
+        template<typename T, typename T2 = engine::WorldManager>
+        bool HasComponent() const
+        {
+            return T2::GetActiveWorld().HasComponent<T>(m_entity);
+        }
+
     protected:
         Entity m_entity;
         bool m_active;
--- Engine/src/Engine/ECS/GameObject.h
+++ Engine/src/Engine/ECS/GameObject.h
@@ -23,42 +23,34 @@ Technology is prohibited.
 
 namespace engine
 {
-    /****************************************************************************//*!
-     @brief     Describes a gameobject which is the basic unit and building block for 
-                every scene. This object provides the ability to tap on existing ECS
-                systems and the ability to have scenegraph.
-    *//*****************************************************************************/
     class GameObject final
     {
     private:
         Entity m_entity;
 
     public:
-        /*
-        Transform3D&    Transform;
-        bool&           ActiveSelf;
-        std::string&    Name;
-        */
-        Transform3D& Transform() const { return GetComponent<Transform3D>(); };
-        bool& ActiveSelf() const { return GetComponent<GameObjectComponent>().ActiveSelf; }
-        std::string& Name() const { return GetComponent<GameObjectComponent>().Name; }
+        
+        // Getters
+        Transform3D& Transform()    const { return GetComponent<Transform3D>(); };
+        bool& ActiveSelf()          const { return GetComponent<GameObjectComponent>().ActiveSelf; }
+        std::string& Name()         const { return GetComponent<GameObjectComponent>().Name; }
+        Entity GetID()              const { return m_entity; }
 
+        // Rule of 5
         GameObject();
         GameObject(GameObject const& copy)        = default;
         GameObject(GameObject &&)                 = default;
         GameObject& operator=(GameObject const&)  = default;
         GameObject& operator=(GameObject&&)       = default;
 
-        //Construct GameObject Based on Entity
+        // Non-Traditional Copy Construct GameObject Based on Entity
         GameObject(Entity entt);
 
-        // use with caution, should not immediately delete the object
+        // use with caution, this should not immediately delete the object!
         void Destroy();
 
         //implicit cast operator
         operator Entity() const { return m_entity; }
-
-        Entity GetID() const { return m_entity; }
 
         void AddChild(GameObject const& gameObj, bool preserveTransforms = false);
         void AddChild(std::initializer_list<GameObject> gameObjs, bool preserveTransforms = false);
@@ -69,38 +61,79 @@ namespace engine
         // CAN BE DONE BUT NOT REQUIRED RIGHT NOW
         //GameObject* FindGameObjectInChildrenByName(std::string const& name);
 
+        /*---------------------------------------------------------------------------------*/
+        /* Queries                                                                         */
+        /*---------------------------------------------------------------------------------*/
         template<typename Component>
-        Component& GetComponent() const;
+        Component& GetComponent() const
+        {
+            return WorldManager::GetActiveWorld().GetComponent<Component>(m_entity);
+        }
 
+        template<typename Component>
+        Component* TryGetComponent() const
+        {
+            return WorldManager::GetActiveWorld().TryGetComponent<Component>(m_entity);
+        }
+
+        template<typename Component>
+        bool HasComponent() const
+        {
+            return WorldManager::GetActiveWorld().HasComponent<Component>(m_entity);
+        }
+
+
+        /*---------------------------------------------------------------------------------*/
+        /* Manipulation                                                                    */
+        /*---------------------------------------------------------------------------------*/
         
-        template<typename Component>
-        Component* TryGetComponent() const;
+        template<typename Derived, typename... Args>
+        std::enable_if_t<std::is_base_of_v<Component, Derived>, Derived& >
+        AddComponent(Args... args) const
+        {
+            return WorldManager::GetActiveWorld().EmplaceComponent<Derived>(m_entity, true, args...);
+        }
+
+        template<typename NotDerived, typename... Args>
+        std::enable_if_t<!std::is_base_of_v<Component, NotDerived>, NotDerived& >
+        AddComponent(Args... args) const
+        {
+            return WorldManager::GetActiveWorld().EmplaceComponent<NotDerived>(m_entity, args...);
+        }
 
         template<typename Component>
-        bool HasComponent() const;
-
-
-        template<typename Component, typename... Args>
-        Component& AddComponent(Args... args) const;
-
-        template<typename Component>
-        Component& AddComponent(Component component) const;
+        Component& AddComponent(Component component) const
+        {
+            return WorldManager::GetActiveWorld().AddComponent<Component>(m_entity, component);
+        }
 
         template <typename Component>
-        Component* AddComponent(GameObject const& src) const;
-
+        Component* AddComponent(GameObject const& src) const
+        {
+            CopyComponent<Component>(src);
+            return TryGetComponent<Component>(m_entity);
+        }
 
         template<typename Component>
-        void RemoveComponent() const;
+        void RemoveComponent() const
+        {
+            WorldManager::GetActiveWorld().RemoveComponent<Component>(entity.uniqueID);
+        }
 
         template<>
         void RemoveComponent<Transform3D>() const { throw "Cannot Remove Transform Component"; }
 
         template<typename Component>
-        bool CopyComponent(GameObject const& src) const;
-        
+        bool CopyComponent(GameObject const& src) const
+        {
+            return WorldManager::GetActiveWorld().CopyComponent<Component>(src.m_entity, m_entity);
+        }
+
         template<typename Component>
-        bool TransferComponent(GameObject const& src) const;
+        bool TransferComponent(GameObject const& src) const
+        {
+            return WorldManager::GetActiveWorld().TransferComponent<Component>(src.m_active, m_entity);
+        }
 
         /*template<typename Component>
         bool AttemptRemoveComponent();
@@ -108,95 +141,12 @@ namespace engine
         template<typename Component>
         Component& AssertComponent();*/
 
-        template<typename Component, typename... Args>
-        Component& EnsureComponent(Args... args) const;
+        template<typename Component, typename...Args>
+        Component& EnsureComponent(Args...args) const
+        {
+            return HasComponent<Component>() ? GetComponent<Component>() : AddComponent<Component>(args...);
+        }
 
     };
-
-    /*---------------------------------------------------------------------------------*/
-    /* Template Implementation                                                         */
-    /*---------------------------------------------------------------------------------*/
-    template<typename Component>
-    Component& GameObject::GetComponent() const
-    {
-        return WorldManager::GetActiveWorld().GetComponent<Component>(m_entity);
-    }
-    
-    template<typename Component>
-    Component* GameObject::TryGetComponent() const
-    {
-        return WorldManager::GetActiveWorld().TryGetComponent<Component>(m_entity);
-    }
-
-    template<typename Component>
-    bool GameObject::HasComponent() const
-    {
-        return WorldManager::GetActiveWorld().HasComponent<Component>(m_entity);
-    }
-
-    template<typename Component, typename...Args>
-    Component& GameObject::AddComponent(Args...args) const
-    {
-        return WorldManager::GetActiveWorld().EmplaceComponent<Component>(m_entity, args...);
-    }
-
-    template<typename Component>
-    Component& GameObject::AddComponent(Component component) const
-    {
-        return WorldManager::GetActiveWorld().AddComponent<Component>(m_entity);
-    }
-
-    template <typename Component>
-    Component* GameObject::AddComponent(GameObject const& src) const
-    {
-        CopyComponent<Component>(src);
-        return TryGetComponent<Component>(m_entity);
-    }
-
-    template<typename Component>
-    void GameObject::RemoveComponent() const
-    {
-        WorldManager::GetActiveWorld().RemoveComponent<Component>(entity.uniqueID);
-    }
-    
-    template<typename Component>
-    bool GameObject::CopyComponent(GameObject const& src) const
-    {
-        return WorldManager::GetActiveWorld().CopyComponent<Component>(src.m_entity, m_entity);
-    }
-
-    template<typename Component>
-    bool GameObject::TransferComponent(GameObject const& src) const
-    {
-        return WorldManager::GetActiveWorld().TransferComponent<Component>(src.m_active, m_entity);
-    }
-
-    /*template<typename Component>
-    bool GameObject::AttemptRemoveComponent()
-    {
-        return ECS_Manager::AttemptRemoveComponent<Component>(entity.uniqueID);
-    }
-
-    template<typename Component>
-    Component& GameObject::AssertComponent()
-    {
-        Component* c = GetComponent<Component>();
-        if (c == nullptr)
-        {
-            std::ostringstream oss;
-            oss << "GameObject does not contain a ";
-            oss << typeid(Component).name();
-            oss << " Component!";
-            throw MissingComponentException(oss.str());
-        }
-        return c;
-        ECS_Manager::TryGetComponent
-    }*/
-
-    template<typename Component, typename...Args>
-    Component& GameObject::EnsureComponent(Args...args) const
-    {
-        return HasComponent<Component>() ? GetComponent<Component>() : AddComponent<Component>(args...);
-    }
 
 }
