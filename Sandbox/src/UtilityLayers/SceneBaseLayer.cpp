bd1cc7bd922eb93ee2df1b5284950ca905d49b32
Lim Guan Hui <l.guanhui@digipen.edu.sg>
2021-09-02 04:55:57 +0800

    scene base layer cpp file created

---
+++ Sandbox/src/UtilityLayers/SceneBaseLayer.cpp
@@ -0,0 +1,101 @@
+/************************************************************************************//*!
+\file       SceneBaseLayer.cpp
+\project    <PROJECT_NAME>
+\author     Lim Guan Hui, l.guanhui , 390009020
+\par        email: l.guanhui\@digipen.edu
+\date       July 28, 2021
+\brief      Base scene layer class
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
+#include "pch.h"
+#include "SceneBaseLayer.h"
+
+
+SceneBaseLayer::SceneBaseLayer(std::string scene_name)
+    : Layer { scene_name }
+    , m_scene { engine::SceneManager::CreateScene(scene_name) }
+{
+}
+
+SceneBaseLayer::~SceneBaseLayer()
+{
+    if(m_scene.IsLoaded()) m_scene.Unload();
+}
+
+engine::Scene::ID_type SceneBaseLayer::GetID() const
+{
+    return m_scene.GetID();
+}
+
+void SceneBaseLayer::OnAttach()
+{
+    engine::SceneManager::SetActiveScene(m_scene.GetID());
+    m_scene.Load();
+    m_scene.GetWorld().RegisterSystem<engine::TransformSystem>();
+    Init();
+}
+
+void SceneBaseLayer::OnDetach() 
+{
+    //engine::SceneManager::SetActiveScene(m_scene.GetID()); -- guaranteed to be exiting correctly
+    Exit();
+    m_scene.Unload();
+}
+
+void SceneBaseLayer::Init() { };
+
+void SceneBaseLayer::Exit() { };
+
+/*********************************************************************************//*!
+\brief    Create a gameobject from the scene
+     
+\return   the created gameobject
+*//**********************************************************************************/
+engine::GameObject SceneBaseLayer::CreateGameObject()
+{
+    return m_scene.CreateGameObject();
+}
+/*********************************************************************************//*!
+\brief    Get the root gameobject from the scene
+     
+\return   root gameobject from the scene
+    
+*//**********************************************************************************/
+engine::GameObject SceneBaseLayer::RootGameObject()
+{
+    return m_scene.GetRoot();
+}
+/*********************************************************************************//*!
+\brief    Saves the scene to its scene file
+     
+    
+*//**********************************************************************************/
+void SceneBaseLayer::SaveScene()
+{
+    m_scene.SaveToFile();
+}
+/*********************************************************************************//*!
+\brief    Saves the scene to a specified scene file
+     
+\param    filename 
+    name of specified scene file
+    
+*//**********************************************************************************/
+void SceneBaseLayer::SaveSceneToFile(std::string const& filename)
+{
+    m_scene.SaveToFileName(filename);
+}
+/*********************************************************************************//*!
+\brief    Sets this layer's scene as the active scene
+     
+    
+*//**********************************************************************************/
+void SceneBaseLayer::SetSceneAsActive()
+{
+    engine::SceneManager::SetActiveScene(m_scene.GetID());
+}
+
--- Sandbox/src/UtilityLayers/SceneBaseLayer.h
+++ Sandbox/src/UtilityLayers/SceneBaseLayer.h
@@ -26,71 +26,45 @@ protected:
 
 public:
 
-    SceneBaseLayer(std::string scene_name)
-        : Layer { scene_name }
-        , m_scene { engine::SceneManager::CreateScene(scene_name) }
-    {
-    }
+    SceneBaseLayer(std::string scene_name);
+    ~SceneBaseLayer();
 
-    ~SceneBaseLayer()
-    {
-        if(m_scene.IsLoaded()) m_scene.Unload();
-    }
+    /*********************************************************************************//*!
+    \brief    Get this scene's id
+     
+    \return   this scene's id
+    
+    *//**********************************************************************************/
+    engine::Scene::ID_type GetID() const;
 
-    engine::Scene::ID_type GetID() const
-    {
-        return m_scene.GetID();
-    }
-
-    void OnAttach() final override
-    {
-        engine::SceneManager::SetActiveScene(m_scene.GetID());
-        m_scene.Load();
-        m_scene.GetWorld().RegisterSystem<engine::TransformSystem>();
-        Init();
-    }
-
-    void OnDetach() final override
-    {
-        //engine::SceneManager::SetActiveScene(m_scene.GetID()); -- guaranteed to be exiting correctly
-        Exit();
-        m_scene.Unload();
-    }
+    void OnAttach() override;
+    void OnDetach() override;
 
 protected:
     
-    virtual void Init() { };
+    virtual void Init();
 
-    virtual void Exit() { };
+    virtual void Exit();
 
     /*********************************************************************************//*!
     \brief    Create a gameobject from the scene
      
     \return   the created gameobject
     *//**********************************************************************************/
-    virtual engine::GameObject CreateGameObject()
-    {
-        return m_scene.CreateGameObject();
-    }
+    virtual engine::GameObject CreateGameObject();
     /*********************************************************************************//*!
     \brief    Get the root gameobject from the scene
      
     \return   root gameobject from the scene
     
     *//**********************************************************************************/
-    virtual engine::GameObject RootGameObject()
-    {
-        return m_scene.GetRoot();
-    }
+    virtual engine::GameObject RootGameObject();
     /*********************************************************************************//*!
     \brief    Saves the scene to its scene file
      
     
     *//**********************************************************************************/
-    virtual void SaveScene()
-    {
-        m_scene.SaveToFile();
-    }
+    virtual void SaveScene();
     /*********************************************************************************//*!
     \brief    Saves the scene to a specified scene file
      
@@ -98,17 +72,11 @@ protected:
         name of specified scene file
     
     *//**********************************************************************************/
-    virtual void SaveSceneToFile(std::string const& filename)
-    {
-        m_scene.SaveToFileName(filename);
-    }
+    virtual void SaveSceneToFile(std::string const& filename);
     /*********************************************************************************//*!
     \brief    Sets this layer's scene as the active scene
      
     
     *//**********************************************************************************/
-    virtual void SetSceneAsActive()
-    {
-        engine::SceneManager::SetActiveScene(m_scene.GetID());
-    }
+    virtual void SetSceneAsActive();
 };
