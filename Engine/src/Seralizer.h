a9828665f51623c40dae7a274cb3886fe53784e7
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2021-09-13 13:47:19 +0800

    commented seralizer

--- Engine/src/Seralizer.h
+++ Engine/src/Seralizer.h
@@ -29,24 +29,97 @@ public:
 
 	Serializer();
 	~Serializer() {};
+	/*********************************************************************************//*!
+	\brief    Load object usually used for prefabs
+	 
+	\param    prefab prefab filename
+	\param    parent the head to attatch to
+	\return   the parent
+	*//**********************************************************************************/
 	static engine::Entity LoadObject(const std::string& prefab,engine::Entity parent = 0);
+	/*********************************************************************************//*!
+	\brief    save prefab
+	 
+	\param    prefab filepath location
+	\param    object start of item to save
+	
+	*//**********************************************************************************/
 	static void SaveObject(const std::string& prefab,engine::Entity object);
+	/*********************************************************************************//*!
+	\brief    Save the whole scene
+	 
+	\param    path - directory to save to
+	\param    scene - the scene file else active scene
+	
+	*//**********************************************************************************/
 	static void SaveWorld(const std::string& path,const engine::Scene& scene);
 	static void SaveWorld(const std::string& path);
+
+	/*********************************************************************************//*!
+	\brief    Load scene
+	 
+	\param    path - path to load from
+	\param    scene - load into scene / else use active scene
+	
+	*//**********************************************************************************/
 	static void LoadWorld(const std::string& path,const engine::Scene& scene);
 	static void LoadWorld(const std::string& path);
 private:
+	/*********************************************************************************//*!
+	\brief    Save hierarchy of object
+	 
+	\param    go - start of object
+	\param    osw - write to file
+	
+	*//**********************************************************************************/
 	static void SaveHierarchy(engine::GameObject& go, rapidjson::PrettyWriter<rapidjson::OStreamWrapper>& osw);
+	/*********************************************************************************//*!
+	\brief    Save object
+
+	\param    go - start of object
+	\param    osw - write to file
+
+	*//**********************************************************************************/
 	static void SaveItem(engine::GameObject& go, rapidjson::PrettyWriter<rapidjson::OStreamWrapper>& osw);
+	
+	
+	/*********************************************************************************//*!
+	\brief    Saves individual component using rttr to reference
+	 
+	\param    component - component to save
+	\param    writer - write to file
+	
+	*//**********************************************************************************/
 	template<typename Component>
 	static void SaveComponent(Component & component, rapidjson::PrettyWriter<rapidjson::OStreamWrapper>& writer);
+	/*********************************************************************************//*!
+	\brief    load component from file using the declared rttr format
+	 
+	\param    arr - rapidjson object containing data
+	\param    component - component to populate
+	
+	*//**********************************************************************************/
 	template<typename Component>
 	static void LoadComponent(rapidjson::Value::Array& arr, Component& component);
 
+	/*********************************************************************************//*!
+	\brief    saving scripting components into file
+	 
+	\param    go - object containing the scripting component
+	\param    writer - write to file
+	
+	*//**********************************************************************************/
 	static void SaveScripts(engine::GameObject& go, rapidjson::PrettyWriter<rapidjson::OStreamWrapper>& writer);
+	/*********************************************************************************//*!
+	\brief    Load script's variables from file 
+	 
+	\param    arr - rapidjson data
+	\param    go - target to write scipting data to
+	
+	*//**********************************************************************************/
 	static void LoadScripts(rapidjson::Value::Array& arr, engine::GameObject& go);
 
-
+	/**!*******************Helper functions for seralizing *********************************/
 	static oom::vec2 GetVec2(rapidjson::Value& val)
 	{
 		auto& arr = val.GetArray();
